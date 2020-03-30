02bb73be6e47ff658f8aaa8062f42c35f8830751
Rafael <trafalgarlaw458@gmail.com>
2020-03-30 20:04:27 -0300

    mobile da aplicacao

---
+++ mobile/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+//react navite nao tem asm mesmas tags do html exceto pelo div nao tem semantica
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes/>
+  );
+}
